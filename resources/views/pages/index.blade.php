cd8d354a07f56f7aaf4266d43fa90ac4765c8336
DanielMalmgren <daniel@kolefors.se>
2020-02-12 08:36:32 +0000

    And even more of them...

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -26,7 +26,7 @@
 @if(count($announcements) > 0)
     <div class="list-group mb-4">
         @foreach($announcements as $announcement)
-            <a href="/announcements/{{$announcement->id}}" class="list-group-item list-group-item-action">
+            <a href="announcements/{{$announcement->id}}" class="list-group-item list-group-item-action">
                 <div class="d-flex w-100 justify-content-between">
                     <h5 class="mb-1">{{$announcement->heading}}</h5>
                     <small>{{\Carbon\Carbon::parse($announcement->created_at)->format('Y-m-d')}}</small>
@@ -38,7 +38,7 @@
 @endif
 
 @can('manage announcements')
-    <a href="/announcements/create" class="btn btn-primary">@lang('Skapa nytt meddelande')</a>
+    <a href="announcements/create" class="btn btn-primary">@lang('Skapa nytt meddelande')</a>
 @endcan
 
 @endsection
